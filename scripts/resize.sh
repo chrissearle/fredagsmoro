f83a29fa6fd69f50bf7d7ddbee71d91ab5b412c3
Chris Searle <chris@chrissearle.org>
2015-11-13 15:09:48 +0100

    Handle PNG in width check too. Apply retroactively

--- scripts/resize.sh
+++ scripts/resize.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for F in `find $1 -name *.jpg -print`; do
+for F in `find $1 \( -name '*.jpg' -o -name '*.png' \) -print`; do
   width=`identify -format %w $F`
   if [ $width -gt 700 ]; then
     echo $F Too big
