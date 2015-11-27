4f81ac76bb9a92e6a338d9d6e895b1edf5f89616
Chris Searle <chris@chrissearle.org>
2015-11-27 15:58:00 +0100

    Only scale width

--- scripts/resize.sh
+++ scripts/resize.sh
@@ -4,8 +4,6 @@ for F in `find $1 \( -name '*.jpg' -o -name '*.png' \) -print`; do
   width=`identify -format %w $F`
   if [ $width -gt 700 ]; then
     echo $F Too big
-    convert $F -resize 700x700\> $F.new
-    rm $F
-    mv $F.new $F
+    convert $F -resize 700x\> $F
   fi
 done
